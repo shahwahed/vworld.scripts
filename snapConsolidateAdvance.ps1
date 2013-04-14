8588cb7af008d927f3f98035d8f5f00c9cd2998f
Shah Mohsin WAHED <shahmohsin.wahed@gmail.com>
2013-04-15 00:13:47 +0200

    modified:   snapConsolidateAdvance.ps1 simplification submit by Timo Sugliani

--- snapConsolidateAdvance.ps1
+++ snapConsolidateAdvance.ps1
@@ -1,73 +1,54 @@
 # -----------------------------------------------------------------------------
-# Script    : consolide snapshot on vm on esx of vcenter
+# Script    : consolide snapshots on vms from a vSphere Host
 # Author    : Shah Mohsin WAHED
 # Date      : 02/04/2013
 # copyright : Copyright 2013, Shah Mohsin WAHED
 # credits   : Shah Mohsin WAHED
 # email     : shahmohsin.wahed@gmail.com
 # maintainer: Shah Mohsin WAHED
-# status    : v1.0
+# status    : v1.1
 # Keywords  : vsphere esx5.x snapshot performance
-# comments  : this script will consolidate snapshot automaticaly (could be launch in schedule manager or other crontab like)
-#             BEWARE it you vm got his vdisk attach to another vm (hotadd backup for example) you cannot consolidate it
-#             This script can be use when you have an ESX host who keep disconnect due to a VM with too much ghost snapshot chained
-#             usefull to fix this vmware kb : http://kb.vmware.com/kb/2009217
+# comments  : this script will consolidate snapshots automaticaly 
+#             (Could be launched through the Windows task scheduler)
+#             WARNING: If your vm got a vdisk attached to another vm 
+#             (hotadd backup for example) you cannot consolidate it. 
+#             This script can be used when you have an ESX host which keeps 
+#             disconnecting due to a VM with too much ghost snapshots chained 
+#             useful against this vmware kb : http://kb.vmware.com/kb/2009217
 #
+# WARNING : do not use on vcloud environnement
+#
+# Version history script simplification submit by Timo Sugliani
 # ----------------------------------------------------------------------------- 
 
-# just to make sure we could launch the script in crontab like
-set-ExecutionPolicy RemoteSigned
+#$vSphereHost = "vSphere.hostname.com" or "192.168.0.101"
+#$username = "Administrator"
+#$password = "Password123"
 
 # add vmware snapin
 Add-PSSnapin VMware.VimAutomation.Core
 
+Write-Host "*****  WARNING do not use on vCloud Environnement *****"
+Read-Host "Press enter to continue"
 
-Write-Host "Welcome to the magical snapshot killer" -foregroundcolor green
+$vSphereHost = read-host "Please enter your vSphere Host (FQDN) or IP address you want to connect to: "
 
-# replace read-host by your esx or vcenter if you want to make it static
-$esx = read-host "Please enter esx name or IP addres (full or short name) you wan't to clean"
+Write-Host "Enter your vSphere Administrator credentials" 
+$username = read-host "Username: "
+$password = read-host -assecurestring "Password: "
 
-# define $userid and $userpass if you doesn't want to ask user/password each time
+# Connect to the vSphere host
+Write-Host "Connecting to " $vSphereHost -foregroundcolor green
 
-#$userid = "uncomment this for static user"
-#$userpass = "uncomment this for static password"
+Connect-VIServer $vSphereHost -User $username -Password $password -WarningAction SilentlyContinue
 
+# Fetch all the VMs which needs consolidation.
+$vms = Get-VM | where-Object { $_.ExtensionData.Runtime.consolidationNeeded }
 
-if(($userid -eq $null) -or ($userid -eq "")){
-
-$userid = read-host "Enter your userid to run script (admin right)"
-
-while (($userid -eq $null) -or ($userid -eq "")) {$userid = read-host "Enter your userid to run script (admin right)"}
-
-$userpass= read-host "Enter your password"
-while (($userpass -eq $null) -or ($userpass -eq "")) {$userpass= read-host "Enter your password (break script if you don't know it :)"}
-
+if ($vms -ne $null) { # if value of $vms is null we have no vm needed consolidation
+    # let start the job cycle from $vms to consolidated all of them!!
+    foreach ($myvm in $vms) {
+        Write-Host $myvm " need consolidate"
+        $myvm.ExtensionData.ConsolidateVMDisks_Task() 
+    }
 }
-
-Write-Host "As you wish!" -foregroundcolor green
-
-#connect to $esx
-
-Write-Host "Connecting to " $esx -foregroundcolor green
-
-Connect-VIServer $esx -User $userid -Password $userpass -WarningAction SilentlyContinue
-
-# we look at vm who need to be consolidated
-$vms = Get-VM | where-Object {$_.ExtensionData.Runtime.consolidationNeeded}
-
-
-if ( $vms -ne $null )
-# if value of $vms is null we have no vm needed consolidation
-{
-#let start the job cycle from $vms to consolidated all of them!!
-	foreach ($myvm in $vms) {
-	    Write-Host $myvm " need consolidate, task launch to take care of that chief!"
-		$myvm.ExtensionData.ConsolidateVMDisks_Task() 
-
-	}
-
-}
-
-# uncomment this line for debug and see what's happen
-#Read-Host "Press anykey to exit"
-
